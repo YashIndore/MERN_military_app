c5708ea1495927313aa01bc81dc058fd20585abb
YashIndore <indoreyash2001@gmail.com>
2023-12-12 23:05:15 +0530

    Inittial Version

---
+++ routes/testRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { testController } = require('../controllers/testController');
+
+
+const router = express.Router();
+
+
+router.get("/", testController);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+//rest config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/test', require('./routes/testRoutes'));
+app.use('/api/v1/auth', require('./routes/authRoutes'));
+app.use('/api/v1/inventory', require('./routes/inventoryRoutes'));
+
+//port
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(
+        `Node server runnning in ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`.bgBlue.white);
+});
